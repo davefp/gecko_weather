71db0f4cfb51d7a1d111948393c6e44713e44cb3
David Underwood <davefp@gmail.com>
2012-11-08 21:29:10 -0500

    adds unit option to weather forecast

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -6,7 +6,8 @@ class WeatherController < ApplicationController
 	respond_to :json
 	
 	def get
-		@raw_weather_data = open("http://weather.yahooapis.com/forecastrss?w=#{params[:id]}&u=c").read
+		puts params
+		@raw_weather_data = open("http://weather.yahooapis.com/forecastrss?w=#{params[:id]}&u=#{params[:unit]}").read
 		@wd = XmlSimple.xml_in(@raw_weather_data, { 'ForceArray' => false })['channel']['item']['condition']
 		#{"item":{"text":"<div class='t-size-x72'>-6&deg;</div><div>Cloudy</div>"}}
 		@weather_text = {'item' => {'text' => "<div class='t-size-x72'>#{@wd['temp']}&deg;</div><div>#{@wd['text']}</div>"}}
@@ -14,7 +15,7 @@ class WeatherController < ApplicationController
 	end
 	
 	def get_scale
-		@raw_weather_data = open("http://weather.yahooapis.com/forecastrss?w=#{params[:id]}&u=c").read
+		@raw_weather_data = open("http://weather.yahooapis.com/forecastrss?w=#{params[:id]}&u=#{params[:unit]}").read
 		@wd = XmlSimple.xml_in(@raw_weather_data, { 'ForceArray' => false })['channel']['item']['condition']
 		@forecast = XmlSimple.xml_in(@raw_weather_data, { 'ForceArray' => false })['channel']['item']['forecast'][0]
 		@weather_text = {'item' => @wd['temp'], 'min' => @forecast['low'], 'max' => @forecast['high']}
